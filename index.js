30e8ea9f021484dd245e095a1fee5e4d0e5ba7a0
norman lewis <takialog@gmail.com>
2018-04-02 16:43:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+function handlebarsSetup() {
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerHelper('displayIngredient', (ingredient) => {return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient.value  + '</li>')})
+
+}
+function initForm() {
+  var formTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
+  document.getElementsByTagName("main")[0].innerHTML += formTemplate()
+}
+
+function createRecipe() {
+  // event.preventDefault();
+  var descriptionText = document.getElementById('description').value
+  var ingredientsNodeList = document.getElementsByName('ingredients')
+  var nameText = document.getElementById('name').value
+  var recipeTemplate = Handlebars.compile(document.getElementById('recipe-template').innerHTML)
+  document.getElementsByTagName("main")[0].innerHTML += recipeTemplate({name: nameText, ingredients: ingredientsNodeList, description: descriptionText})
+}
+
+function displayEditForm() {
+  var descriptionText = document.getElementById('recipeDescription').value
+  var ingredientsNodeList = document.getElementsByName('ingredientsList')
+  var nameText = document.getElementById('recipeName').value
+  var formTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
+  
+}
 function init() {
   //put any page initialization/handlebars initialization here
+  initForm();
+  handlebarsSetup();
 }
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+  init();
+
 })
